2519e793f34f7ec84a184d9f73a0a9ccc23da8e6
ZJihane <jihanezouahri@gmail.com>
2024-10-15 12:46:30 +0100

    final frontend V1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,28 @@
-import {Routes , Route} from 'react-router-dom' ;
-import './App.css'
-import Header from './components/Header'
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Chat from './pages/Chat';
-import NotFound from './pages/NotFound';
-import { useAuth } from './context/AuthContext';
-
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+import { useAuth } from "./context/AuthContext";
 function App() {
- console.log(useAuth()?.isLoggedIn);
+  const auth = useAuth();
 
   return (
-   <main>
-        <Header/>
-        <Routes>
+    <main>
+      <Header />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={<Chat />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
         <Route path="*" element={<NotFound />} />
       </Routes>
-   </main>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,18 +1,36 @@
-import { Box, Typography, Button } from "@mui/material";
-import React from "react"
+import React, { useEffect } from "react";
 import { IoIosLogIn } from "react-icons/io";
+import { Box, Typography, Button } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
-
+import { toast } from "react-hot-toast";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-     
-    const handleSubmit = () => {
-
+  const navigate = useNavigate();
+  const auth = useAuth();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    try {
+      toast.loading("Signing In", { id: "login" });
+      await auth?.login(email, password);
+      toast.success("Signed In Successfully", { id: "login" });
+    } catch (error) {
+      console.log(error);
+      toast.error("Signing In Failed", { id: "login" });
     }
-
-    return (
-       <Box width={"100%"} height={"100%"} display="flex" flex={1}>
-        <Box padding={6} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
-        <img src="logo.png" alt="Robot" style={{ width: "450px" , height: "400px" }} />
+  };
+  useEffect(() => {
+    if (auth?.user) {
+      return navigate("/chat");
+    }
+  }, [auth]);
+  return (
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+        <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
       </Box>
       <Box
         display={"flex"}
@@ -73,7 +91,6 @@ const Login = () => {
       </Box>
     </Box>
   );
-    
-}
+};
 
-export default Login 
+export default Login;
